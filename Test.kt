b2ae45a79ad0187a05c17180d0b681f66de9c8f4
DeVenLucaz <ypatole035@gmail.com>
2026-06-09 01:08:28 +0530

    Chore: Remove temporary python patch scripts and Test.kt

--- Test.kt
+++
@@ -1,9 +0,0 @@
-val fillerWords = setOf("how", "do", "i", "can", "where", "is", "the", "a", "an", "to", "open", "find", "show", "me", "please", "tap", "click", "press", "button", "icon", "app", "on", "at", "in", "it")
-fun extractKeywords(query: String): Pair<String, String> {
-    val lowerCaseQuery = query.lowercase()
-    val words = lowerCaseQuery.split("\\s+".toRegex())
-    val keywords = words.filter { it.isNotBlank() && !fillerWords.contains(it) }
-    val extracted = keywords.joinToString(" ")
-    return Pair(query, extracted)
-}
-println("'" + extractKeywords("camera ").second + "'")
