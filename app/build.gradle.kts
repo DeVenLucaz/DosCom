f4087038723234ac5a8a8b82ea80e051797d7ccd
DeVenLucaz <ypatole035@gmail.com>
2026-06-09 15:00:54 +0530

    Update versionCode and versionName in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.devenlucaz.doscom"
         minSdk = 26
         targetSdk = 33
-        versionCode = 2
-        versionName = "2.0.0"
+        versionCode = 3
+        versionName = "2.4.0"
     }
 
     signingConfigs {
